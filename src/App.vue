6c12db0bb063804584a120574e78d1a06e974f56
cp-20 <n.naoki.1587@gmail.com>
2025-07-09 03:16:53 +0900

    reproduction

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import TheWelcome from './components/TheWelcome.vue';
+
+const handleHelloWorldClick = () => {
+  console.log('HelloWorld component clicked!');
+};
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did it!" @click="handleHelloWorldClick" />
     </div>
   </header>
 
